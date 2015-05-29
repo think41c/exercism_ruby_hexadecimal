5442f856877f144dc4307041419a927f47829406
Tony DiNitto <think41c@gmail.com>
2015-05-29 09:45:14 -0700

    Cleaned code up.

--- hexadecimal.rb
+++ hexadecimal.rb
@@ -1,21 +1,20 @@
 class Hexadecimal
   def initialize(hex)
     @length_of_hex = hex.length
-    @hex = hex.upcase#.to_i
+    @hex = hex.upcase
     @hex_to_dec = { "1" => 1, "2" => 2, "3" => 3, 
-      "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8,
-      "9" => 9, "A" => 10, "B" => 11, "C" => 12,
-                   "D" => 13, "E" => 14, "F" => 15 }
-
+                    "4" => 4, "5" => 5, "6" => 6, 
+                    "7" => 7, "8" => 8, "9" => 9, 
+                    "A" => 10,"B" => 11,"C" => 12, 
+                    "D" => 13,"E" => 14,"F" => 15 
+                  }
   end
 
   def to_decimal
     counter = 0 
     
     until counter >= @length_of_hex
-      puts @hex
-      result = @hex_to_dec[@hex]
-      # result = 16**counter * hex_to_dec[@hex]
+      result = 16**counter * @hex_to_dec[@hex]
       counter += 1 
     end
     result
