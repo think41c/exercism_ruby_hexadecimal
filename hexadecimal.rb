8d0762cd07cceea685b2d8218eef844ff38a2467
Tony DiNitto <think41c@gmail.com>
2015-05-28 23:29:58 -0700

    Actual basic logic for first test passing.

--- hexadecimal.rb
+++ hexadecimal.rb
@@ -1,10 +1,17 @@
 class Hexadecimal
   def initialize(hex)
+    @hex = hex.to_i
+    hex_to_dec = { A: 10, B: 11, C: 12, 
+                   D: 13, E: 14, F: 15 }
+    puts hex_to_dec[:A]
+
   end
 
-
   def to_decimal
-    1
+    16**0 * @hex
   end
 
 end
+
+a = Hexadecimal.new(1)
+
