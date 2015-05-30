d3c7995ada225e7cf6227b1bfcba33a936d36f81
Tony DiNitto <think41c@gmail.com>
2015-05-29 17:49:14 -0700

    More tests passing.

--- hexadecimal_test.rb
+++ hexadecimal_test.rb
@@ -21,12 +21,10 @@ class HexadecimalTest < Minitest::Test
   end
 
   def test_hex_100_is_decimal_256
-    skip
     assert_equal 256, Hexadecimal.new('100').to_decimal
   end
 
   def test_hex_19ace_is_decimal_105166
-    skip
     assert_equal 105_166, Hexadecimal.new('19ace').to_decimal
   end
 
